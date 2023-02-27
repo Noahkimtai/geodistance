4da0b9db90d09fcaa86eb1e20dd46a812ade4eba
Noahkimtai <noahkimtai24@gmail.com>
2023-02-27 09:59:53 +0300

    feat: add form to collect user travel information added to client

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,17 @@ import react from 'react'
 function Home(){
     return(
         <div>
-            Hello world!
+            Calculate distance, cost and travel time.
+            <form className='p-2'>
+                <input type='text' placeholder='Enter your pointer of origin'></input>
+                <input type='text' placeholder='Enter your final destination'></input>
+                <select className ="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5">
+                    <option value='Air'>Air</option>
+                    <option value='Water'>Water</option>
+                    <option value='Road'>Road</option>
+                    <option value='Rail'>Rail</option>
+                </select>
+            </form>
         </div>
     )
 }
